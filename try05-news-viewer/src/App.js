c028d929d0d836ab24f0fba7eff866c79787e6a1
cch5980 <cch5980@gmail.com>
2022-01-07 20:12:00 +0900

    News Viewer 만들기

--- try05-news-viewer/src/App.js
+++ try05-news-viewer/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+import NewsList from './components/NewsList'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null)
+  const handleClick = async () => {
+    // axios 데이터 불러오기
+    // axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+    //   setData(response.data)
+    // })
+    try {
+      const response = await axios.get(
+        // 'https://jsonplaceholder.typicode.com/todos/1',
+        'https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=d4b5739cc29c4a03b36dae1e6fb2f7a7'
+      )
+      setData(response.data)
+    } catch(e) {
+      console.log(e)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <div>
+        <button onClick={handleClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} /> } */}
+      <NewsList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
